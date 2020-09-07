ca5f6fce4963a65629ef3f789baa7d38b78ec1e3
DennekJr <ohirikennedy@yahoo.com>
2020-09-07 21:42:28 +0200

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,63 @@
 import React from "react";
-import {Layout, Header, Navigation, Content, Footer, FooterSection, FooterLinkList, Button} from "react-mdl";
+import {
+  Layout,
+  Header,
+  Navigation,
+  Content,
+  Footer,
+  FooterSection,
+  FooterLinkList,
+  Button,
+} from "react-mdl";
 import "./App.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Main from "./components/route/main";
-import {Home} from "./components/home/home";
+import { Home } from "./components/home/home";
 
 function App() {
-    return (
-        <div>
-            <Layout>
-                <section>
-                    <Header
-                        className="headerColor"
-                        title={
-                            <Link to="/" style={{textDecoration: "none", color: "white"}}>
-                                Austen Paul
-                            </Link>
-                        }
-                        scroll
-                    >
-                        <Navigation>
-                            <a href="/product_photography">Product Photography</a>
-                            <a href="/about">About</a>
-                            <a href="/contact">Contact</a>
-                        </Navigation>
-                    </Header>
-                        {/*<Home/>*/}
-                </section>
-                <Content>
-                    <div className="page-content">
-                        <Main/>
-                    </div>
-                </Content>
-                <Footer size="mini">
-                    <FooterSection type="right" className="footer">
-                        <FooterLinkList style={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%'}}>
-                            <Button href="#" style={{position: 'relative', right: '50%', backgroundColor: '#5BABAB', color: 'white'}}>Let's Connect</Button>
-                            <h6>© 2020 by DennekJr Productions</h6>
-                        </FooterLinkList>
-                    </FooterSection>
-                </Footer>
-            </Layout>
-        </div>
-    );
+  return (
+    <div>
+      <Layout>
+        <section>
+          <Header
+            className="headerColor"
+            title={
+              <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                Austen Paul
+              </Link>
+            }
+            scroll
+          >
+            <Navigation>
+              <a href="/product_photography">Product Photography</a>
+              <a href="/about">About</a>
+              <a href="/contact">Contact</a>
+            </Navigation>
+          </Header>
+          {/*<Home/>*/}
+        </section>
+        <Content>
+          <div className="page-content">
+            <Main />
+          </div>
+        </Content>
+        <section>
+          <div className="footer">
+            <Button
+              href="#"
+              style={{
+                backgroundColor: "#5BABAB",
+                color: "white",
+              }}
+            >
+              Let's Connect
+            </Button>
+            <h6>© 2020 by DennekJr Productions</h6>
+          </div>
+        </section>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
